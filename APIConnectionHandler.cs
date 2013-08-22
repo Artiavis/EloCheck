3a0b7b7c26783cbd4a80614a06461cffd4c4ee2c
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2013-08-22 15:56:55 -0400

    Begin adding JSON parsing

--- APIConnectionHandler.cs
+++ APIConnectionHandler.cs
@@ -2,30 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace EloCheck
 {
     class APIConnectionHandler
     {
 
-        static readonly APIConnectionHandler APIConnection 
+        public static APIConnectionHandler APIConnection
         {
-            private set; 
-            public get; 
+            get { return _conn; }
         }
 
-        bool IsConnected { private set; public get; }
+        static readonly APIConnectionHandler _conn = new APIConnectionHandler();
+        public bool IsConnected { private set; get; }
         private const string IDENT = "BOBDOLEISMYHERO";
         private LeagueAPI api;
-        private List<BitmapImage> rankedChamps;
+        public List<BitmapImage> RankedChamps { get; private set; }
 
         public APIConnectionHandler()
         {
-            APIConnection = new APIConnectionHandler();
-            rankedChamps = new List<BitmapImage>();
+            RankedChamps = new List<BitmapImage>();
             api = new LeagueAPI(IDENT);
             IsConnected = api.Connect();
         }
@@ -40,13 +42,101 @@ namespace EloCheck
         {
             // TODO perform this task using TPL
             string result = api.ApiRequest("summoner", name, region);
+            // TODO make this processing a callback of the previous task
+            PlayerStats stats = CompleteSummonerRequest(result);
             return result;
         }
 
-        // TODO implement this
-        private void CompleteSummonerRequest(string title)
+        // TODO implement this using JsonSerializer.Deserialize and JReader
+        private PlayerStats CompleteSummonerRequest(string json)
         {
-            
+            JsonSerializer ser = new JsonSerializer();
+            PlayerStats playerStats = ser.Deserialize<PlayerStats>(new JReader(new StringReader(json)));
+            return playerStats;
+            // TODO Implement Ranked Stats and Match History
         }
+
+        private void BuildChampions(string player, string json, string portraitsPath)
+        {
+            //        ScrollPane list = (ScrollPane) parent.lookup("#rankedChampList");
+            //        AnchorPane anchor = (AnchorPane)list.lookup("#rankedChampions");
+            RankedChamps.Clear();
+            if (json == null)
+                return;
+            String[] champions = json.Split('_');
+            // TODO sort array?
+            int numChamps = champions.Length;
+
+            // rankedChampClicked ??
+            int i = 0, k = 0;
+            for (; i < numChamps; i++)
+            {
+                string lCaseName = Regex.Replace(champions[i], "[^A-Za-z]", "").ToLowerInvariant();
+                portraitsPath += lCaseName + "_square_0.png";
+                Uri uri = new Uri(portraitsPath);
+                BitmapImage portrait = new BitmapImage(uri);
+                RankedChamps.Add(portrait);
+            }
+        }
+
+        public bool StatsReqeust(string name, string region)
+        {
+            // TODO implement stats request
+            return false;
+        }
+
+        public bool ChampionStatsRequest(string name, string champion, string region)
+        {
+            // TODO implement champion specific statistics reqeust
+            return false;
+        }
+
+        public void GameRequest(string name, string region)
+        {
+            // TODO make this request using TPL
+            string result = api.ApiRequest("game", name, region);
+        }
+
+        // TODO implement this
+        public void CompleteGameRequest(string title)
+        {
+
+        }
+
+        private class PlayerStats
+        {
+            bool Ranked { get; set; }
+            Dictionary<String, Object> Ranked_Stats { get; set; }
+            string Ranked_Champions { get; set; }
+            string Tier { get; set; }
+            string Division { get; set; }
+            int Wins { get; set; }
+            int Losses { get; set; }
+            string S1 { get; set; }
+            string S2 { get; set; }
+            string S3 { get; set; }
+        }
+
+        /// <summary>
+        /// Extend the JsonTextReader class to provide a version
+        /// which corrects for key names with spaces.
+        /// </summary>
+        private class JReader : Newtonsoft.Json.JsonTextReader
+        {
+            public JReader(TextReader r)
+                : base(r)
+            { }
+
+            public override bool Read()
+            {
+                bool b = base.Read();
+                if (base.CurrentState == State.Property && ((string)base.Value).Contains(' '))
+                {
+                    base.SetToken(JsonToken.PropertyName, ((string)base.Value).Replace(' ', '_'));
+                }
+                return b;
+            }
+        }
+
     }
 }
